1123cc454dad94228cab074f1ad8395982084124
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-17 18:12:38 +0000

    can filter by tag, test passes

--- server.rb
+++ server.rb
@@ -28,5 +28,11 @@ class Server < Sinatra::Base
     redirect to('/')
   end
 
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
   run! if app_file == $0
 end
