6ef87a45fd0b8decc966ce1be53fbf7a75aef9f8
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-18 11:41:56 +0000

    can add user - no password confirmation

--- server.rb
+++ server.rb
@@ -5,12 +5,17 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 
   require './lib/link'
+  require './lib/user'
+  
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class Server < Sinatra::Base
 
+  
+  enable :sessions
+  set :session_secret, 'super secret'
   set :views, Proc.new { File.join(root, "/views") }
 
   get '/' do
@@ -34,5 +39,25 @@ class Server < Sinatra::Base
     erb :index
   end
 
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(:email => params[:email],
+                :password => params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
+  end
+
   run! if app_file == $0
 end
+
+
+
