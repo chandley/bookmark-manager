a7394f0e3a988332de09e6dbb14f61a47db6ca39
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-20 15:10:02 +0000

    working css

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require_relative '../../lib/user'
-
+require_relative '../../app/helpers/session'
 
 feature "User signs up" do
 
@@ -9,7 +9,6 @@ feature "User signs up" do
     # sign_up
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")
-
   end
 
   scenario "with a password that doesn't match" do
@@ -34,4 +33,53 @@ feature "User signs up" do
     fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
+
 end
+
+feature "User signs in" do
+
+include SessionHelpers
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario "with correct credentials" do
+    visit ('/')
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content("Welcome, test@test.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit ('/')
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'wrong')
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+  
+end
+ 
+feature 'User signs out' do
+
+  include SessionHelpers
+
+  before(:each) do
+    User.create(:email => 'test@test.com',
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in('test@test.com', 'test')
+    click_button 'Sign out'
+    expect(page).to have_content("Good bye!")
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+end
+
+
